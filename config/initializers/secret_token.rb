bbbf453ce023cae1bb9a62377a24e204ba15c9c3
Charles Pobre <cdpobre@yahoo.com>
2013-09-27 15:15:00 +0800

    invitable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitsapp::Application.config.secret_key_base = '00065cb5e76ec4959a3e9f04c56cb1d873c459147debfbac37d14dd0db6fac26bd23823848b445a38317abb9e33f209c6aa60625cef02e760d646f4eaf06a5fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bitsapp::Application.config.session_store :cookie_store, key: '_bitsapp_session'
